872f57740bf4753095af7b683b2624093044091c
좌동철 <vyvydkf628@naver.com>
2020-07-13 21:20:06 +0900

    fix lint

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Switch, Router, Route} from 'react-router-dom'
-import Note from './components/pages/Notes'
+import { Switch, Router, Route } from 'react-router-dom';
+import Note from './components/pages/Notes';
 import { createBrowserHistory } from 'history';
 
 const App = () => (
   <div>
-    <Router history={createBrowserHistory()} >
+    <Router history={createBrowserHistory()}>
       <Switch>
         <Route path="/notes/:id/" exact component={Note} />
       </Switch>
