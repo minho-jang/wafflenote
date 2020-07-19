2d3a41d32e990120c9cf038e1746732d105986c3
좌동철 <vyvydkf628@naver.com>
2020-07-19 15:29:54 +0900

    Feat: Add package for redux-thunk

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, compose, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
 import reducers from './reducers';
 import App from './App';
 
