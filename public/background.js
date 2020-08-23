9f1db038e9c2f9a0f12391eb6f7443b23da4f252
박운상 <iloveunhs@naver.com>
2020-08-23 22:01:01 +0900

    New features of Popup.js, Add stop/pause button

--- public/background.js
+++ public/background.js
@@ -1,17 +1,24 @@
-import { requestScreenSharing, cancelScreenSharing } from './screen/screenShare.js';
+import {
+  requestScreenSharing,
+  cancelScreenSharing,
+} from "./screen/screenShare.js";
 
 chrome.browserAction.onClicked.addListener(function (activeTab) {
-  chrome.tabs.create({ url: chrome.extension.getURL('popup.html') }, function (tab) {});
+  chrome.tabs.create({ url: chrome.extension.getURL("popup.html") }, function (
+    tab
+  ) {});
 });
 
 chrome.runtime.onConnect.addListener((port) => {
   port.onMessage.addListener((msg) => {
-    if (msg.type === 'waffleNoteStart') {
+    if (msg.type === "waffleNoteStart") {
       requestScreenSharing(port, msg);
-    } else if (msg.type === 'changeSlide') {
+    } else if (msg.type === "changeSlide") {
       // Defind method about slide changing
-    } else if (msg.type === 'waffleNoteEnd') {
+    } else if (msg.type === "waffleNoteStop") {
       cancelScreenSharing(msg);
+    } else if (msg.type === "waffleNotePause") {
+      // Pause
     }
   });
 });
--- src/components/pages/Popup.js
+++ src/components/pages/Popup.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
+import { getState } from "../../apis/storage";
 
 import CaptureButton from "./CaptureButton";
+import StopButton from "./StopButton";
 import { getLastCapturedImage } from "../../apis/storage";
 import waffleLogo from "../../static/waffleLogo.png";
 
@@ -29,7 +31,23 @@ const Button = styled(Link)`
   }
 `;
 
-const Info = styled.div`
+const InfoTrue = styled.div`
+  margin-top:50px;
+  margin-left:px
+  width: 171px;
+  height: 18px;
+  font-family: NotoSansKR;
+  font-size: 12px;
+  font-weight: normal;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1.5;
+  letter-spacing: normal;
+  text-align: center;
+  color: #ffffff;
+`;
+
+const InfoFalse = styled.div`
   margin-top:10px;
   margin-left:50px
   width: 171px;
@@ -61,7 +79,16 @@ const Container = styled.div`
   background-color: #ffbc3e;
 `;
 
-const Image = styled.img`
+const ImageTrue = styled.img`
+  margin-top: 4px;
+  margin-left: 35px;
+  width: 200px;
+  height: 140px;
+  box-shadow: 3px 3px 6px 0 rgba(0, 0, 0, 0.16);
+  background-color: #ffffff;
+`;
+
+const ImageFalse = styled.img`
   margin-top: 69px;
   margin-left: 35px;
   width: 200px;
@@ -70,8 +97,26 @@ const Image = styled.img`
   background-color: #ffffff;
 `;
 
+const Time = styled.div`
+margin-top:10px;
+margin-left:120px
+width: 30px;
+height: 18px;
+font-family: NotoSansKR;
+font-size: 12px;
+font-weight: normal;
+font-stretch: normal;
+font-style: normal;
+line-height: 1.5;
+letter-spacing: normal;
+text-align: center;
+color: #ffffff;
+`;
+
 const Popup = () => {
   const [curSlide, setCurSlide] = useState({});
+  const [curState, setCurState] = useState(false);
+
   useEffect(() => {
     setImage();
     setInterval(async () => {
@@ -85,18 +130,40 @@ const Popup = () => {
     setCurSlide(response);
   };
 
-  return (
-    <Container>
-      <Title src={waffleLogo} />
-      {curSlide.id}
-      <Image src={curSlide.slide} />
-      <Info>현재 탭에서 강의를 듣고 계신가요?</Info>
-      <CaptureButton />
-      <Button to="/notes/1" target="_blank">
-        와플노트 바로가기
-      </Button>
-    </Container>
-  );
+  getState().then((state) => {
+    setCurState(state);
+  });
+
+  if (curState === true) {
+    // 와플노트 실행중
+    return (
+      <Container>
+        <Title src={waffleLogo} />
+        {curSlide.id}
+        <InfoTrue>현재 슬라이드</InfoTrue>
+        <ImageTrue src={curSlide.slide} />
+        <Time> 00:00 </Time>
+        <StopButton />
+        <Button to="/notes/1" target="_blank">
+          요약중인 노트보기
+        </Button>
+      </Container>
+    );
+  } else {
+    // 와플노트 종료
+    return (
+      <Container>
+        <Title src={waffleLogo} />
+        {curSlide.id}
+        <ImageFalse src={curSlide.slide} />
+        <InfoFalse>현재 탭에서 강의를 듣고 계신가요?</InfoFalse>
+        <CaptureButton />
+        <Button to="/notes/1" target="_blank">
+          와플노트 바로가기
+        </Button>
+      </Container>
+    );
+  }
 };
 
 export default Popup;
