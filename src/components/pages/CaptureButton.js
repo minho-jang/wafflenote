f61d547b1c32b9419956e81087a0f980cfc1e57f
박운상 <iloveunhs@naver.com>
2020-08-23 17:30:03 +0900

    Popup CSS

---
+++ src/components/pages/CaptureButton.js
@@ -0,0 +1,35 @@
+import React from "react";
+import styled from "styled-components";
+
+const Button = styled.button`
+  margin-top: 24px;
+  margin-left: 35px;
+  width: 200px;
+  height: 30px;
+  border-radius: 18px;
+  box-shadow: 3px 3px 6px 0 rgba(0, 0, 0, 0.16);
+  background-color: #fdeed0;
+  border: 0px;
+  outline: 0px;
+  font-family: NotoSansKR;
+  font-size: 14px;
+  font-weight: 500;
+  font-stretch: normal;
+  font-style: normal;
+  color: #ffbc3e;
+  cursor: pointer;
+`;
+
+const port = chrome.runtime.connect({
+  name: "WaffleNote",
+});
+
+const CaptureButton = (props) => {
+  const startCapture = () => {
+    port.postMessage({ type: "waffleNoteStart", text: "start" }, "*");
+  };
+
+  return <Button onClick={startCapture}>강의 시작</Button>;
+};
+
+export default CaptureButton;
--- src/components/pages/Popup.js
+++ src/components/pages/Popup.js
@@ -1,22 +1,73 @@
-import React, { useState, useEffect } from 'react';
-import styled from 'styled-components';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 
-import CaptureButton from '../../CaptureButton';
-import { getLastCapturedImage } from '../../apis/storage';
+import CaptureButton from "./CaptureButton";
+import { getLastCapturedImage } from "../../apis/storage";
+import waffleLogo from "../../static/waffleLogo.png";
+
+const Button = styled(Link)`
+  margin-top: 12px;
+  margin-left: 35px;
+  width: 200px;
+  height: 30px;
+  border-radius: 18px;
+  box-shadow: 3px 3px 6px 0 rgba(0, 0, 0, 0.16);
+  background-color: #fdeed0;
+  border: 0px;
+  outline: 0px;
+  text-align: center;
+  font-family: NotoSansKR;
+  font-size: 14px;
+  font-weight: 500;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 30px;
+  color: #ffbc3e;
+  &:hover: {
+    color: #ffbc3e;
+  }
+`;
+
+const Info = styled.div`
+  margin-top:10px;
+  margin-left:50px
+  width: 171px;
+  height: 18px;
+  font-family: NotoSansKR;
+  font-size: 12px;
+  font-weight: normal;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1.5;
+  letter-spacing: normal;
+  text-align: center;
+  color: #ffffff;
+`;
+
+const Title = styled.img`
+  width: 103px;
+  height: 16px;
+  margin-top: 12px;
+  margin-left: 11px;
+`;
 
 const Container = styled.div`
   display: flex;
   flex-direction: column;
-  width: 320px;
+  width: 270px;
   height: 430px;
-  align-items: center;
+  box-shadow: 3px 3px 6px 0 rgba(0, 0, 0, 0.16);
+  background-color: #ffbc3e;
 `;
 
 const Image = styled.img`
-  margin: 10px;
-  width: 90%;
-  height: 40%;
+  margin-top: 69px;
+  margin-left: 35px;
+  width: 200px;
+  height: 140px;
+  box-shadow: 3px 3px 6px 0 rgba(0, 0, 0, 0.16);
+  background-color: #ffffff;
 `;
 
 const Popup = () => {
@@ -29,19 +80,21 @@ const Popup = () => {
   }, []);
 
   const setImage = async () => {
-    const response = await getLastCapturedImage('note');
-    
+    const response = await getLastCapturedImage("note");
+
     setCurSlide(response);
   };
 
   return (
     <Container>
+      <Title src={waffleLogo} />
       {curSlide.id}
       <Image src={curSlide.slide} />
-      <Link to="/notes/1" target="_blank" className="ui button">
-        Go to classroom
-      </Link>
+      <Info>현재 탭에서 강의를 듣고 계신가요?</Info>
       <CaptureButton />
+      <Button to="/notes/1" target="_blank">
+        와플노트 바로가기
+      </Button>
     </Container>
   );
 };
