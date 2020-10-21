6aeff299df8b5e6622555541dabee01542d9a589
장민호 <minho9301@naver.com>
2020-10-21 20:12:09 +0900

    Feat: modify server url

--- src/apis/waffle.js
+++ src/apis/waffle.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 const waffle = axios.create({
-  baseURL: process.env.BASE_URL || 'http://13.124.80.162:3000',
+  baseURL: process.env.BASE_URL || 'http://15.165.43.178:3000',
 });
 export {waffle};
