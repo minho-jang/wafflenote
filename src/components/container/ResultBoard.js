0798386e5c6e48d46d3af1d92d1281e33153d948
박운상 <iloveunhs@naver.com>
2020-11-03 12:36:54 +0900

    Modify resultboard design

--- src/components/container/ResultBoard.js
+++ src/components/container/ResultBoard.js
@@ -1,24 +1,22 @@
-import React, { useState, useEffect } from 'react';
-import styled from 'styled-components';
-import { connect } from 'react-redux';
-import TitleText from '../presenter/TitleText';
-import { getSlides, editSlide } from '../../actions/slides';
-import { editNote, getNoteResult } from '../../actions/notes';
-import ContentBoard from '../presenter/ContentBoard';
-import { getResult } from '../../apis/utils';
-import Graph from './Graph';
-import PDFMaker from './PDFMaker';
-import TitleBoard from '../presenter/TitleBoard';
-import Spinner from '../presenter/Spinner';
-import PDFButton from '../presenter/PDFButton';
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import { connect } from "react-redux";
+import TitleText from "../presenter/TitleText";
+import { getSlides, editSlide } from "../../actions/slides";
+import { editNote, getNoteResult } from "../../actions/notes";
+import SentenceLank from "../presenter/SentenceLank";
+import { getResult } from "../../apis/utils";
+import Graph from "./Graph";
+import PDFMaker from "./PDFMaker";
+import TitleBoard from "../presenter/TitleBoard";
+import Spinner from "../presenter/Spinner";
+import PDFButton from "../presenter/PDFButton";
 
 const Body = styled.div`
   margin: 0 auto;
 `;
 
-
 const ResultBoard = (props) => {
-
   const [isRequested, toggleRequest] = useState(false);
   useEffect(() => {
     props.getNoteResult(props.noteId);
@@ -31,24 +29,30 @@ const ResultBoard = (props) => {
     return <Spinner />;
   }
   const onClick = () => {
-    toggleRequest(true)
-    PDFMaker(props.slides,props.note.summary, props.note.title)
-      .then((res) => {
-        toggleRequest(false);
-      })
+    toggleRequest(true);
+    PDFMaker(props.slides, props.note.summary, props.note.title).then((res) => {
+      toggleRequest(false);
+    });
   };
 
   return (
     <Body>
-      {isRequested 
-        ? <Spinner message={"PDF를 추출 중입니다."}/>
-        : <PDFButton onClick={onClick} />}
-      
+      {isRequested ? (
+        <Spinner message={"PDF를 추출 중입니다."} />
+      ) : (
+        <PDFButton onClick={onClick} />
+      )}
       <TitleBoard slide={props.note} onTitleSubmit={onNoteTitleSubmit} />
-      <TitleText>강의 요약노트</TitleText>
-      <ContentBoard input={props.note.summary ? props.note.summary : '요약된 스크립트가 없습니다'} title="스크립트" />
-
-      <TitleText>분석</TitleText>
+      <TitleText>와플노트의 PICK! - 핵심 문장</TitleText>
+      <SentenceLank
+        input={
+          props.note.key_sentences
+            ? props.note.key_sentences
+            : "요약된 스크립트가 없습니다"
+        }
+        title="스크립트"
+      />
+      <TitleText>와플노트의 PICK! - 핵심 키워드</TitleText>
       <Graph data={props.note.note_keywords ? props.note.note_keywords : []} />
     </Body>
   );
@@ -61,4 +65,6 @@ const mapStateToProps = (state, ownProps) => {
   };
 };
 
-export default connect(mapStateToProps, { getSlides, editNote, getNoteResult })(ResultBoard);
+export default connect(mapStateToProps, { getSlides, editNote, getNoteResult })(
+  ResultBoard
+);
